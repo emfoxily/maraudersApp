49918a738ad6517c15cee7c3923028d67fea0196
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-10-11 15:43:01 -0400

    created sock seed

---
+++ models/socks.js
@@ -0,0 +1,68 @@
+let sockSeed = [
+  {
+    name: 'Not Again!',
+    img: '../images/sock1.jpg',
+    price: 25,
+    qty: 10,
+    inStock: true,
+    description: 'These socks are great for road trips and all the bathroom stops you make along the way!'
+  },
+  {
+    name: 'It\'s...Complicated',
+    img: '../images/sock2.jpg',
+    price: 25,
+    qty: 4,
+    inStock: true,
+    description: 'Wear your heart on your socks! Let people know that you\'re complicated and also thank them for reading your socks!'
+  },
+  {
+    name: 'I\'m Just Trollin\'!',
+    img: '../images/sock3.jpg',
+    price: 30,
+    qty: 12,
+    inStock: true,
+    description: 'Troll your friends with these eccentric socks! You\'ll certainly be the center of attention!'
+  },
+  {
+    name: 'I Regret This...',
+    img: '../images/sock4.jpg',
+    price: 25,
+    qty: 0,
+    inStock: false,
+    description: 'Blast off into your next mistake with these out of this world socks!'
+  },
+  {
+    name: 'I\'m A Unicorn!',
+    img: '../images/sock5.jpg',
+    price: 15,
+    qty: 10,
+    inStock: true,
+    description: 'Pronounce your identity as a unicorn with these magical socks! You\'ll certainly be one of a kind.'
+  },
+  {
+    name: 'Leave Me Alone',
+    img: '../images/sock6.jpg',
+    price: 15,
+    qty: 2,
+    inStock: true,
+    description: 'Let people know that you aren\'t social with these lonely socks! No one will bother you when you\'re wearing these!'
+  },
+  {
+    name: 'I Don\'t Care',
+    img: '../images/sock7.jpeg',
+    price: 15,
+    qty: 17,
+    inStock: true,
+    description: 'Put everyone on snooze with these rude socks! No one will want to talk to you as long as you\'re in these socks!'
+  },
+  {
+    name: 'Let\'s Get Weird',
+    img: '../images/sock8.jpg',
+    price: 20,
+    qty: 5,
+    inStock: true,
+    description: 'These socks are anything but boring! Who wants to be normal anymore anyways?'
+  }
+]
+
+module.exports = sockSeed
