7f9ccd7f3113361fd285f796414b0f857eb3879b
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-10-11 14:22:33 -0400

    created sock schema

---
+++ models/sockSchema.js
@@ -0,0 +1,34 @@
+// mongoose!
+const mongoose = require('mongoose')
+// schema!
+const Schema = mongoose.Schema
+
+// sock schema!
+const sockSchema = Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  img: String,
+  price: {
+    type: Number,
+    min: 0,
+    required: true
+  },
+  qty: {
+    type: Number,
+    min: 0,
+    required: true
+  },
+  inStock: {
+    type: Boolean,
+    default: true
+  },
+  description: String
+})
+
+// model!
+const Sock = mongoose.model('Sock', sockSchema)
+
+// export!
+module.exports = Sock;
