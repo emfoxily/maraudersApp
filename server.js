c04bf38668e5638328ce8703f52d8a1ab269ef1e
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-10-11 18:40:53 -0400

    displayed socks on index page

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const app = express()
 const db = mongoose.connection;
+const session = require('express-session')
+const Socks = require('./models/sockSchema.js')
+const sockSeed = require('./models/socks.js')
 
 // port!
 const PORT = process.env.PORT || 3000
@@ -14,33 +17,54 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/' + 'maraude
 // connect to mongo!
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 
-// error / success
+// error / success!
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-// open the connection to mongo
+// open the connection to mongo!
 db.on('open', () => {
   console.log('Mischief Managed.');
 })
 
-// middleware
+// middleware!
 // use public folder for static assets
 app.use(express.static('public'))
 
 // populates req.body with parsed info from forms, if no data from forms, it will return an empty object
 app.use(express.urlencoded({extended: false}))
 
-// use method override
+// use method override!
 // use PUT and DELETE verbs (HTML only allows GET and POST)
 app.use(methodOverride('_method'))
 
-// routes
+// routes!
+// index / sock store!
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // // if the current user is a wizard, show wand shop
+  // if (req.session.currentUser) {
+  //   Wands.find({}, (error, allWands) => {
+  //     res.render('wandShop/index.ejs')
+  //   })
+  // } else {
+  // else, show the sock shop
+    Socks.find({}, (error, allSocks) => {
+      res.render('index.ejs', {
+        socks: allSocks
+      })
+    })
+  // }
 })
 
-// listen
+// Socks.create( sockSeed, (error, data) => {
+//   if (error) console.log(error.message);
+//   console.log('added sock data');
+// })
+
+// show!
+app.get('/:id')
+
+// listen!
 app.listen(PORT, () => {
   console.log('I solemnly swear that I am up to no good...');
 })
