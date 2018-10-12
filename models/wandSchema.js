9673538ae18fb865b47ab02c600283901bc4bbca
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-10-12 10:23:27 -0400

    created wand schema

---
+++ models/wandSchema.js
@@ -0,0 +1,34 @@
+// mongoose!
+const mongoose = require('mongoose')
+// schema!
+const Schema = mongoose.Schema
+
+// wand schema!
+const wandSchema = Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  img: String,
+  price: {
+    type: Number,
+    min: 0,
+    required: true
+  },
+  description: String,
+  qty: {
+    type: Number,
+    min: 0,
+    required: true
+  },
+  inStock: {
+    type: Boolean,
+    default: true
+  }
+})
+
+// model!
+const Wand = mongoose.model('Wand', wandSchema)
+
+// export!
+module.exports = Wand
