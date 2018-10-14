e01a7bf8569b7f59410049753a2d7391f73510b5
emily reno <emilyreno@emilys-MacBook-Pro.local>
2018-10-13 21:01:53 -0400

    created wand seed

--- models/wands.js
+++ models/wands.js
@@ -11,7 +11,7 @@ const wandSeed = [
   {
     name: 'Dogwood Wand with Phoenix Feather Core',
     img: '../images/wands/dogwoodWand.png',
-    length: '13 1/2 inches',
+    length: '13 ½ inches',
     price: 200,
     description: 'Dogwood wands are quirky and mischievous; they have playful natures and insist upon partners who can provide them with scope for excitement and fun. It would be quite wrong, however, to deduce from this that dogwood wands are not capable of serious magic when called upon to do so; they have been known to perform outstanding spells under difficult conditions, and when paired with a suitably clever and ingenious witch or wizard, can produce dazzling enchantments. An interesting foible of many dogwood wands is that they refuse to perform non-verbal spells and they are often rather noisy.',
     qty: 27,
@@ -34,7 +34,43 @@ const wandSeed = [
     description: 'It is said that a laurel wand cannot perform a dishonourable act, although in the quest for glory (a not uncommon goal for those best suited to these wands), I have known laurel wands perform powerful and sometimes lethal magic. Laurel wands are sometimes called fickle, but this is unfair. The laurel wand seems unable to tolerate laziness in a possessor, and it is in such conditions that it is most easily and willingly won away. Otherwise, it will cleave happily to its first match forever, and indeed has the unusual and engaging attribute of issuing a spontaneous lightning strike if another witch or wizard attempts to steal it.',
     qty: 11,
     inStock: true
-  }
+  },
+  {
+    name: 'Elder Wand with Phoenix Feather Core',
+    img: '../images/wands/elderWand.png',
+    length: '14 inches',
+    price: 1000,
+    description: 'The rarest wand wood of all, and reputed to be deeply unlucky, the elder wand is trickier to master than any other. It contains powerful magic, but scorns to remain with any owner who is not the superior of his or her company; it takes a remarkable wizard to keep the elder wand for any length of time. The old superstition, ‘wand of elder, never prosper,’ has its basis in this fear of the wand, but in fact, the superstition is baseless, and those foolish wandmakers who refuse to work with elder do so more because they doubt they will be able to sell their products than from fear of working with this wood. The truth is that only a highly unusual person will find their perfect match in elder, and on the rare occasion when such a pairing occurs, I take it as certain that the witch or wizard in question is marked out for a special destiny. An additional fact that I have unearthed during my long years of study is that the owners of elder wands almost always feel a powerful affinity with those chosen by rowan.',
+    qty: 0,
+    inStock: false
+  },
+  {
+    name: 'Sycamore Wand with Dragon Heartstring Core',
+    img: '../images/wands/sycamoreWand.png',
+    length: '9 ½ inches',
+    price: 800,
+    description: 'The sycamore makes a questing wand, eager for new experience and losing brilliance if engaged in mundane activities. It is a quirk of these handsome wands that they may combust if allowed to become ‘bored’, and many witches and wizards, settling down into middle age, are disconcerted to find their trusty wand bursting into flame in their hand as they ask it, one more time, to fetch their slippers. As may be deduced, the sycamore’s ideal owner is curious, vital and adventurous, and when paired with such an owner, it demonstrates a capacity to learn and adapt that earns it a rightful place among the world’s most highly-prized wand woods.',
+    qty: 7,
+    inStock: true
+  },
+  {
+    name: 'Poplar Wand with Unicorn Hair Core',
+    img: '../images/wands/poplarWand.png',
+    length: '11 inches',
+    price: 600,
+    description: '‘If you seek integrity, search first among the poplars,’ was a great maxim of my grandfather, Gerbold Ollivander, and my own experience of poplar wands and their owners tallies exactly with his. Here is a wand to rely upon, of consistency, strength and uniform power, always happiest when working with a witch or wizard of clear moral vision. There is a tired old joke among lesser wandmakers that no poplar wand has ever chosen a politician, but here they show their lamentable ignorance: two of the Ministry’s most accomplished Ministers for Magic, Eldritch Diggory and Evangeline Orpington, were the possessors of fine, Ollivander-made poplar wands.',
+    qty: 7,
+    inStock: true
+  },
+  {
+    name: 'Rowan Wand with Phoenix Feather Core',
+    img: '../images/wands/rowanWand.png',
+    length: '10 ½ inches',
+    price: 800,
+    description: 'Rowan wood has always been much-favoured for wands, because it is reputed to be more protective than any other, and in my experience renders all manner of defensive charms especially strong and difficult to break. It is commonly stated that no dark witch or wizard ever owned a rowan wand, and I cannot recall a single instance where one of my own rowan wands has gone on to do evil in the world. Rowan is most happily placed with the clear-headed and the pure-hearted, but this reputation for virtue ought not to fool anyone – these wands are the equal of any, often the better, and frequently out-perform others in duels.',
+    qty: 7,
+    inStock: true
+  },
 ]
 
 module.exports = wandSeed
